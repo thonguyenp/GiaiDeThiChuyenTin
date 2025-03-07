daa7ba7b107e9780f25dd5ba47f15d40382113b1
thonguyenp <nphuoctho2406@gmail.com>
2025-03-07 23:13:01 +0700

    Hoàn thành bài 4 đề LQD_NT 23

--- LQDNhaTrang/2023/MinThuaSoNguyenTo.cpp
+++ LQDNhaTrang/2023/MinThuaSoNguyenTo.cpp
@@ -1,5 +1,15 @@
 #include<stdio.h>
 
+//Tìm Thừa SNT nhỏ nhất của 1 số
+int ThuaSNTNhoNhat (int n)
+{
+    if (n < 2) return -1;
+    if (n % 2 ==0) return 2;
+    for (int i = 3; i * i <= n; i+=2)
+        if (n % i == 0) return i; //trả về snt đầu tiên tìm được
+    return n; //n chính là 1 số nguyên tố
+}
+
 int main ()
 {
     long long n, k;
@@ -13,11 +23,11 @@ int main ()
     {
         for (int j = 2; j <= k; j++)
         {
-            if (j % a[i] == 0)
-                res[i]++;
+            if (ThuaSNTNhoNhat(j) == a[i])  //nếu thừa số ngto của j bằng với số a[i]
+                res[i]++;   //res ở vị trí 1 tăng thêm 1
         }
     }
-
+    
     for (int j = 0; j < n; j++)
         printf("%d\n",res[j]);
 }
