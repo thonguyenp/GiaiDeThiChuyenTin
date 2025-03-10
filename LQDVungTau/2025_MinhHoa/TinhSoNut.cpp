0acad47bf4fbdd4adaba65fcd3eb6464436ac574
thonguyenp <nphuoctho2406@gmail.com>
2025-03-10 11:54:15 +0700

    Hoàn thành review code Bài 2 LQD VT 25 Minh họa

--- LQDVungTau/2025_MinhHoa/TinhSoNut.cpp
+++ LQDVungTau/2025_MinhHoa/TinhSoNut.cpp
@@ -28,10 +28,21 @@ int TongChuSo (int n)
     }
     return n;
 }
-
+/*
+Cách 2: 1 số N hệ 10 có thể được viết là: N = a(K+1) * 10^(K+1) + ... + a1 * 10 ^1 + a0 * 10^0 (k biểu thị cho số a là chữ số hàng nào)
+vd: 123 = 1*100 + 2*10 + 3+10^0
+Giả sử: N = a(K+1)a(K)a(K-1) => Ta lại có: S(N) = a(K+1) + a(K) + a(K-1) 
+vd: 123 = 1 + 2 + 3
+=> Kết quả của n%9 bằng tổng các chữ số của n chia lấy dư cho 9
+=> Vd: 123 % 9 = 6, 1+2+3 = 6 % 9 = 6
+=> Số nút của 1 số sẽ là số đó chia lấy dư cho 9
+Lại có vấn đề: Nút của những số chia hết cho 9 sẽ bằng 9, nhưng nếu dùng công thức trên sẽ bằng 0
+=> Ta lấy n trừ cho 1 để chia dư 9 còn 8, sau đó lấy kết quả cộng thêm 1 => công thức cuối cùng là: 1 + (n-1) % 9
+ */
 int DigitalRoot(int n) {
     if (n == 0) return 0;
-    return 1 + (n - 1) % 9;
+    // return 1 + (n - 1) % 9;
+    return n % 9;
 }
 
 int main ()
@@ -46,4 +57,5 @@ int main ()
         s += DigitalRoot(i);
     }
     printf("%d",s);
+    // printf("%d",DigitalRoot(17));
 }
