7114ae7df2cb7b0a358d81f16285b429dbdd88eb
thonguyenp <nphuoctho2406@gmail.com>
2025-07-27 10:50:57 +0700

    Hoàn thành giảng giải bài Tổng SCP

--- LQDNhaTrang/2024/TongSCPGrok.cpp
+++ LQDNhaTrang/2024/TongSCPGrok.cpp
@@ -1,17 +1,31 @@
 #include <stdio.h>
 #include <math.h>
 
+/*
+    Kiến thức cần nhớ:
+    - Công thức tính bình phương từ 1 -> m
+    - TÌm kiếm nhị phân:
+        + Cách đặt left + (right - left) / 2 để tránh tràn số
+        + Phương pháp tìm kiếm nhị phân
+*/
+
 // Hàm tính tổng 1^2 + 2^2 + ... + m^2
 unsigned long long sum_of_squares(unsigned long long m) {
     return (m * (m + 1) * (2 * m + 1)) / 6;
 }
 
-// Tìm m lớn nhất sao cho sum_of_squares(m) <= x
+// Tìm m lớn nhất sao cho sum_of_squares(m) <= x (dùng binary search)
 unsigned long long find_max_m(unsigned long long x) {
-    unsigned long long left = 0, right = 1000000; // sqrt(10^18) ~ 10^9
+    unsigned long long left = 0, right = x;
     unsigned long long result = 0;
     while (left <= right) {
-        unsigned long long mid = left + (right - left) / 2;
+        //left + (right - left) / 2 tốt hơn (left + right)/2 vì (left + right) có thể bị tràn số
+        unsigned long long mid = left + (right - left) / 2; 
+        /*
+        Nguyên tắc tìm kiếm nhị phân:
+        - Nếu tổng bình phương từ 1 đến mid <= x => số hơi nhỏ => tăng biên trái lên mid+1
+        - Nếu tổng bình phương từ 1 đến mid > x => số hơi lớn => tăng biên phải về mid-1
+        */
         if (sum_of_squares(mid) <= x) {
             result = mid;
             left = mid + 1;
@@ -27,8 +41,8 @@ int main() {
 
     // Đọc n
     scanf("%lu", &n);
-    unsigned long long x[n];
 
+    unsigned long long x[n];
     // Đọc mảng x
     for (unsigned long i = 0; i < n; i++) {
         scanf("%llu", &x[i]);
